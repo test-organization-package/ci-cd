ddec1e7c6a287c894ec635a85c46dcb5d898908c
RachamallaVivek <vivek.rachamalla@zemosolabs.com>
2024-02-08 10:26:14 +0530

    first commit

---
+++ ci-cd-bend/src/main/java/com/example/cicdbend/CiCdBendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdbend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdBendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdBendApplication.class, args);
+	}
+
+}
---
+++ ci-cd-bend/src/test/java/com/example/cicdbend/CiCdBendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdbend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdBendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
